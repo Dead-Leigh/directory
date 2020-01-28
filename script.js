f3752fecfaa706c8d1e53df0f30386638e2e7185
lacarr04@gmail.com <leighcarr@Leighs-MacBook-Pro.local>
2020-01-28 17:00:05 -0500

    did somethings

--- script.js
+++ script.js
@@ -27,20 +27,29 @@ $('.submit-btn').click(function(e) {
   let first = $('#firstname').val();
   let last = $('#lastname').val();
   let email = $('#email').val();
-  let phonenumber = $('#phonenumber').val();
+  let phoneNumber = $('#phonenumber').val();
+  phoneNumber = toNumber(phoneNumber);
+  // Call the function, pass in the phone number, store in tempVar
+  if (phoneNumber.length == '10') {
+    var obj = {};
+    obj.first = first;
+    obj.last = last;
+    obj.email = email;
+    obj.phoneNumber = phoneNumber;
+    console.log(obj);
 
-  var obj = {};
-  obj.first = first;
-  obj.last = last;
-  obj.email = email;
-  obj.phonenumber = phonenumber;
-  console.log(obj);
+    // let newarray = ["first", "last", "email", "phonenumber"];
 
-  // let newarray = ["first", "last", "email", "phonenumber"];
+    myGobalArray.push(obj);
+    console.log(myGobalArray);
+    addTo();
+  } else {
+    alert('phone number wrong format');
+  }
+  // If tempVar does not equal 10, alert("please enter a valid phone")
 
-  myGobalArray.push(obj);
-  console.log(myGobalArray);
-  addTo();
+  // Everything below
+
 
 });
 
@@ -60,17 +69,64 @@ function addTo() {
 
     `);
 
-    $('.delbnt').click(function(e){
-      let pos = $('#' + this.id);
+
 
       // splice that position out of myGobalArray
-      
+
 
       // call addto function
-    });
-  }
 
+  }
 }
+
+//the delete button is being added dynamically so it can not be a
+// .onclick like i tried to do.
+$('.box').on('click', '.delbnt', function(e){
+ // splice that position out of myGobalArray
+ myGobalArray.splice(this.id,1);
+ // call addto function
+ addTo();
+});
+
+
+
+/*
+TO DO:
+create an empty str variahle
+for loop
+  if its a number (there is a method)
+    pushto empy()
+return new string
+
+
+we went to https://repl.it/ to work through the following funtions
+*/
+
+function toNumber (str) {
+  let newStr = "";
+  for(var x = 0; x < str.length; x++){
+    let tempVar = parseInt(str[x]);
+    if(Number.isInteger(tempVar)){
+      newStr+= tempVar;
+    }
+  }
+  return newStr;
+}
+
+
+
+
+
+
+// After you implement the above code.
+// There are still some final touches you can work on.
+// Add some if else logic to make sure they are filling in all fields,
+//  the phone number is only ten digits and a number
+      //HTML - maxlength="10"
+      //HTML - pattern="\d{10}" - should have limited the phone number to just
+      //numbers but it did not work
+//  and sort the myGlobalArray by last name before rendering it to the page.
+//  This project is looking great, you are doing terrific!!
 
 
 /*
